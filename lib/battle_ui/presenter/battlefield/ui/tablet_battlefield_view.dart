8f2f2abc014fc275f31f6b7ea92787f0eb5d5f9a
Igor MIranda <igor_midev@outlook.com>
2022-11-13 00:00:23 -0300

    Created ui of user stats (time and mana)

--- lib/battle_ui/presenter/battlefield/ui/tablet_battlefield_view.dart
+++ lib/battle_ui/presenter/battlefield/ui/tablet_battlefield_view.dart
@@ -12,43 +12,7 @@ class TabletBattlefieldView extends StatelessWidget {
       body: ListView(
         children: const [
           SizedBox(height: 32),
-          // Align(
-          //   child: Container(
-          //     height: 100,
-          //     width: 800,
-          //     decoration: BoxDecoration(
-          //       borderRadius: BorderRadius.circular(20),
-          //       color: const Color.fromARGB(38, 177, 177, 177),
-          //     ),
-          //     child: Row(
-          //       mainAxisAlignment: MainAxisAlignment.center,
-          //       children: [
-          //         SizedBox(
-          //           width: 130,
-          //           child: Column(
-          //             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          //             crossAxisAlignment: CrossAxisAlignment.stretch,
-          //             children: [
-          //               ElevatedButton(
-          //                 onPressed: () {},
-          //                 child: const Text('Surrender'),
-          //               ),
-          //               ElevatedButton(
-          //                 onPressed: () {},
-          //                 child: const Text('Offer draw'),
-          //               ),
-          //               ElevatedButton(
-          //                 onPressed: () {},
-          //                 child: const Text('Pass turn'),
-          //               ),
-          //             ],
-          //           ),
-          //         ),
-          //       ],
-          //     ),
-          //   ),
-          // ),
-
+          PlayerInfoDisplay(),
           SizedBox(height: 20),
           Center(
             child: StageCoordenateGrid(
@@ -67,6 +31,90 @@ class PlayerInfoDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Align(
+      child: Container(
+        height: 100,
+        width: 800,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: const Color.fromARGB(38, 177, 177, 177),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            // ┌─────────────────────────────────────────────────────────
+            // │ The possible movements dots of the selected piece
+            // └─────────────────────────────────────────────────────────
+            const UserDisplay(),
+
+            // ┌─────────────────────────────────────────────────────────
+            // │ The Buttons
+            // └─────────────────────────────────────────────────────────
+            SizedBox(
+              width: 130,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // IconButton(onPressed: () {}, icon: const Icon(Icons.draw)),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Surrender'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Offer draw'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Pass turn'),
+                  ),
+                ],
+              ),
+            ),
+
+            const UserDisplay(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class UserDisplay extends StatelessWidget {
+  const UserDisplay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.centerRight,
+      children: [
+        Container(
+          height: 40,
+          width: 200,
+          padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: const Color.fromARGB(37, 133, 133, 133),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text('zNeutro'),
+              Text('Current mana: 8'),
+            ],
+          ),
+        ),
+        Container(
+          height: 40,
+          width: 80,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: const Color.fromARGB(36, 99, 99, 99),
+          ),
+          child: const Center(child: Text('18:50')),
+        ),
+      ],
+    );
   }
 }
